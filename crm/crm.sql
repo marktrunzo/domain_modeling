37c822cfef9c08b32c9ded0cea896a83657c1aa9
Mark Trunzo <mark.trunzo@kellogg.northwestern.edu>
2023-04-14 21:15:49 +0000

    edits

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,51 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS sales_members;
+
 -- CREATE TABLES
+
+CREATE TABLE contacts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone_number TEXT,
+    company_id INTEGER
+);
+
+CREATE TABLE activities(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    contact_type TEXT,
+    date TEXT,
+    notes TEXT,
+    contact_id INTEGER,
+    salesmember_id INTEGER
+);
+
+CREATE TABLE companies(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE industries(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE relationships(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    industry_id INTEGER,
+    company_id INTEGER
+);
+
+CREATE TABLE sales_members(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,41 @@
 
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS friends;
+
+CREATE TABLE posts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    picture TEXT,
+    time TEXT,
+    user_id INTEGER
+);
+
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    username TEXT,
+    location TEXT
+);
+
+CREATE TABLE likes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    post_id INTEGER
+);
+
+CREATE TABLE comments(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    comment TEXT,
+    post_id INTEGER,
+    user_id INTEGER
+);
+
+CREATE TABLE friends(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    user_id_follower INTEGER
+);
